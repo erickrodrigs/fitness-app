08e0eb9abcbdd22c14e83ab67e47b37593b29f4f
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-06 01:39:35 -0300

    start list-item component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/health/shared/shared.module.ts
+++ src/health/shared/shared.module.ts
@@ -2,15 +2,25 @@ import { CommonModule } from "@angular/common";
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { AngularFireDatabaseModule } from "angularfire2/database";
+
+// services
 import { MealsService } from "./services/meals/meals.service";
 
+// components
+import { ListItemComponent } from "./components/list-item/list-item.component";
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListItemComponent
+  ],
   imports: [
     CommonModule,
     RouterModule,
     AngularFireDatabaseModule
   ],
+  exports: [
+    ListItemComponent
+  ]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
