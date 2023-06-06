8512312f2c69f46e351d46d7d5ee411d32bc30d3
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-06 01:21:49 -0300

    add meal-form component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -10,6 +10,9 @@ import { SharedModule } from "../shared/shared.module";
 import { MealsComponent } from "./containers/meals/meals.component";
 import { MealComponent } from "./containers/meal/meal.component";
 
+// components
+import { MealFormComponent } from "./components/meal-form/meal-form.component";
+
 export const ROUTES: Routes = [
   { path: '', component: MealsComponent },
   { path: 'new', component: MealComponent },
@@ -18,7 +21,8 @@ export const ROUTES: Routes = [
 @NgModule({
   declarations: [
     MealsComponent,
-    MealComponent
+    MealComponent,
+    MealFormComponent
   ],
   imports: [
     CommonModule,
