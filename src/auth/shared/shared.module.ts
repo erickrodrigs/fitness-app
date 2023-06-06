07984e3f87ce3377721cd2d172f7411c00ceb8af
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-05 22:27:54 -0300

    add auth guard
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/auth/shared/shared.module.ts
+++ src/auth/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { ModuleWithProviders, NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AuthFormComponent } from "./components/auth-form/auth-form.component";
 import { AuthService } from "./services/auth/auth.service";
+import { AuthGuard } from "./guards/auth.guard";
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ export class SharedModule {
     return {
       ngModule: SharedModule,
       providers: [
-        AuthService
+        AuthService,
+        AuthGuard
       ]
     }
   }
